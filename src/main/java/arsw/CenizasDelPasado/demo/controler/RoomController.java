599c7284b808ae6302aed4af856d89d230478686
SebasZaUr <Sebasssele2008@gmail.com>
2024-03-08 11:49:20 -0500

    Listo unirse salas

--- src/main/java/arsw/CenizasDelPasado/demo/controler/RoomController.java
+++ src/main/java/arsw/CenizasDelPasado/demo/controler/RoomController.java
@@ -1,15 +1,16 @@
 package arsw.CenizasDelPasado.demo.controler;
 
+import arsw.CenizasDelPasado.demo.exception.CenizasDelPasadpException;
 import arsw.CenizasDelPasado.demo.model.Room;
 import arsw.CenizasDelPasado.demo.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,25 +21,40 @@ public class RoomController {
     RoomService rs;
 
     @PostMapping(value = "/create")
-    public ResponseEntity<?> manageCreateRoom(){
-        try{
+    public ResponseEntity<?> manageCreateRoom() throws CenizasDelPasadpException {
+        try {
             Room room = new Room();
             rs.addNewRoom(room);
-            return new ResponseEntity<>(room,HttpStatus.CREATED);
-        }catch (Exception ex){
+            ArrayList<Room> sala = new ArrayList<>();
+            sala.add(room);
+            return new ResponseEntity<>(sala, HttpStatus.ACCEPTED);
+        } catch (Exception ex) {
             Logger.getLogger(Room.class.getName()).log(Level.SEVERE, null, ex);
-            return new ResponseEntity<>("Error al obtener los planos: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>("Error al crear la sala: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
     }
 
     @GetMapping(value ="/list")
-    public ResponseEntity manageGetAllRooms(){
+    public ResponseEntity<?> manageGetAllRooms(){
         try{
-            return new ResponseEntity<>(rs.getAllRooms(),HttpStatus.ACCEPTED);
+            ArrayList<Room> rooms = rs.getAllRooms();
+            return new ResponseEntity<>(rooms,HttpStatus.ACCEPTED);
         }catch (Exception ex){
             Logger.getLogger(Room.class.getName()).log(Level.SEVERE, null, ex);
             return new ResponseEntity<>("Error al obtener los planos: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(value = "/getRoom")
+    public  ResponseEntity<?> manageGetRoom(@RequestParam String codigo) {
+        try {
+            ArrayList<Room> sala = new ArrayList<>();
+            sala.add(rs.getRoomCode(codigo));
+            return new ResponseEntity<>(sala, HttpStatus.ACCEPTED);
+        } catch (Exception ex) {
+            Logger.getLogger(Room.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("Error al crear la sala: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
